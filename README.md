151d0b6b9e48ce40ec158003c911dd1c20e0328d
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-09-25 23:03:14 -0400

    ready to test

--- README.md
+++ README.md
@@ -1,18 +1,48 @@
-# pi-hat-node-red
+# Simple Raspberry Pi - weather reporter ( Temp and Humidity )
 
 
+![Sense HAT image](./pi-Sense-HAT.png)
 
-## on Raspberry Pi with hat
+
+## pre-reqs
+Raspberry Pi 2 or 3 - not sure about v4
+[Sense HAT](https://www.adafruit.com/product/2738)
+
+### The Sense HAT is an add-on board for the Raspberry Pi
+
+The board allows you to make measurements of temperature, humidity, pressure, and orientation, 
+and to output information using its built-in LED matrix.
+
+We will use it to publish Temperature(Centigrade) and Humidity metrics to [Node-RED](https://nodered.org/) on the [IBM Cloud](https://cloud.ibm.com) - free account!
+
+## This instruction set uses the Raspbian OS 
+* setting up pi with [NOOBS](https://www.raspberrypi.org/documentation/installation/noobs.md)
+## setup the Sense HAT
+* setting up [Sense HAT](https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat)
+
+## setup a cron job as root on Raspberry Pi with a SenseHat
 ```sh
+#become root user
 sudo su
 crontab -e
-# add the following line to POST updates every 5 minutes
-
-5 * * * * /usr/bin/python /home/pi/dev/pushSenseData/push.py >> /home/pi/sense.log 2>&1
+# add the following line to POST updates every 5 or any interval you like, I chose 5 minutes
+echo "5 * * * * /usr/bin/python /home/pi/dev/pi-hat-node-red/push.py >> /home/pi/sense.log 2>&1"
+# restart cron
+systemctl restart cron
 
 exit
-#back to pi userpw
-mkdir -p /home/pi/dev/pushSenseData
+#revert to pi user
+mkdir -p /home/pi/dev && cd /home/pi/dev
 
+#clone repo
+git clone https://github.com/Grant-Steinfeld/pi-hat-node-red.git
+cd pi-hat-node-red
+ls
+stat push.py
+echo "push.py should be here"
 ```
+
+### copy the contents of Pi-SenseHat-Ingress.Node-RED.json to your clipboard and paste into your node red flow
+Note your hostname and update the `NODE_RED_HOST_NAME` variable in push.py with your actual hostname 
+
 
--- push.py
+++ push.py
@@ -5,8 +5,9 @@ import datetime as dt
 from common import *
 sense = SenseHat()
 
-#REPLACE XYZ WITH your nodeRed domain name
-NODE_RED_POST_URI = "https://XYZ.mybluemix.net/sense-hat"
+#REPLACE YOURHOSTNAME WITH your nodeRed domain name
+NODE_RED_HOST_NAME = "YOURHOSTNAME"
+NODE_RED_POST_URI = "https://{}.mybluemix.net/sense-hat".format(NODE_RED_HOST_NAME)
 
 def run():
 	print("running")
@@ -25,8 +26,8 @@ def run():
 
 
 def callNodeRED(args):
-    	print(args)
-	r = requests.post(NODE_RED_POST_URI, data = args)
+    print(args)
+    r = requests.post(NODE_RED_POST_URI, data = args)
 	print(r.status_code)
 	print(r.json())
 
@@ -34,29 +35,29 @@ def getTimeStamp():
     return dt.datetime.now().strftime("%Y-%m-%d %H:%M")
 
 def getTemp():
-        sense.clear()
+    sense.clear()
 
 	temp = sense.get_temperature()
 	sense.clear()
-        bg = (0,0,0)
-        c = (255,255,255)
+    bg = (0,0,0)
+    c = (255,255,255)
 
-        if (temp >= 30.00):
-            print("hot!!")
-            c = (255,0,0) #red
-            bg = (242,231,102) #yellow
-        elif (temp < 30 and temp > 15):
-            print("moderate")
-            c= (3,255,53) #greenw
-            bg = (5,5,5) #dark grey`
-        else:
-            print("cold")
-            c=(3,32,252)
-            bg=(225,225,230)
+    if (temp >= 30.00):
+        print("hot!!")
+        c = (255,0,0) #red
+        bg = (242,231,102) #yellow
+    elif (temp < 30 and temp > 15):
+        print("moderate")
+        c= (3,255,53) #greenw
+        bg = (5,5,5) #dark grey`
+    else:
+        print("cold")
+        c=(3,32,252)
+        bg=(225,225,230)
 
-        sense.show_message("{} {}".format(str(temp).split('.')[0],"C"), text_colour=c, back_colour=bg, scroll_speed=0.9)
-        sleep(2)
-        sense.clear()
+    sense.show_message("{} {}".format(str(temp).split('.')[0],"C"), text_colour=c, back_colour=bg, scroll_speed=0.9)
+    sleep(2)
+    sense.clear()
 	return temp
 
 def getHumidity():
